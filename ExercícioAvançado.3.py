e4a203d6ee662ed20943bf5c46756ad8bb480692
maducorreia <maduardacorreia@gmail.com>
2024-09-30 11:14:28 -0300

    Add files via upload

---
+++ ExercícioAvançado.3.py
@@ -0,0 +1,12 @@
+a = ["", "", "", "", "", "", "", "", "", ""]
+m = ["", "", "", "", "", "", "", "", "", ""]
+
+tamanho = len(a)
+for i in range(tamanho):
+    a[i] = int(input("Digite 10 números: "))
+x = int(input("Digite mais um número: "))
+for y in range(tamanho):
+    m[y] = a[y] * x
+print(a)
+print(x)
+print(m)
