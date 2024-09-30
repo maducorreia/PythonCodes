dd37c81a845d20b4dc08caae0aa0ef6da582418a
maducorreia <maduardacorreia@gmail.com>
2024-09-30 11:38:10 -0300

    Add files via upload

---
+++ ExercícioAvançado.4.py
@@ -0,0 +1,6 @@
+vetor = [0,0,0,0,0]
+tamanho = len(vetor)
+for i in range(tamanho):
+    vetor[i] = int(input("Digite 5 números: "))
+for x in range(tamanho-1, -1, -1):
+    print(vetor[x], end = " ")
